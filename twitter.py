8e02326d1400c1c76c162f9d0d5c7b40adcb8be7
Elkin Garcia <elkingarcia.11@gmail.com>
2023-05-17 15:49:24 -0400

    added twitter func

--- twitter.py
+++ twitter.py
@@ -13,18 +13,6 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 # Create an API object
 api = tweepy.API(auth)
 
-# Define a function to get a random sentence from a paragraph
-def get_random_sentence(paragraph):
-  sentences = paragraph.split(".")
-  return random.choice(sentences)
-
 # Define a function to tweet a random sentence from a paragraph
-def tweet_random_sentence(paragraph):
-  sentence = biblejson.get_random_sentence(paragraph)
+def tweet_random_sentence(sentence):
   api.update_status(sentence)
-
-# Get the paragraph of input from the user
-paragraph = input("Enter a paragraph of text: ")
-
-# Tweet the random sentence
-tweet_random_sentence(paragraph)
