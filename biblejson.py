f2ce50b0f0773d80d83e7531b2e91e18e631081d
Elkin Garcia <elkingarcia.11@gmail.com>
2023-05-17 19:10:07 -0400

    consolidated json

--- biblejson.py
+++ biblejson.py
@@ -1,44 +1,17 @@
 import json
 import random
 
-# 31103 Verses
-books = []
+# 5202 Verses
+verses = []
 
 # Load verses isaiah from json file
-with open("/app/assets/isaiah.json", "r") as f:
+with open("/app/assets/verses.json", "r") as f:
   d = json.load(f)
-resultset = d["verses"]
-books.append(resultset)
+verses = d["verses"]
 
-# Load verses philip from json file
-with open("/app/assets/philip.json", "r") as f:
-  d = json.load(f)
-resultset = d["verses"]
-books.append(resultset)
+print(len(verses))
 
-# Load verses proverbs from json file
-with open("/app/assets/proverbs.json", "r") as f:
-  d = json.load(f)
-resultset = d["verses"]
-books.append(resultset)
-
-# Load verses psalms from json file
-with open("/app/assets/psalms.json", "r") as f:
-  d = json.load(f)
-resultset = d["verses"]
-books.append(resultset)
-
-# Load verses romans from json file
-with open("/app/assets/romans.json", "r") as f:
-  d = json.load(f)
-resultset = d["verses"]
-books.append(resultset)
-
-for book in books:
-  print(len(book))
-  
 # Retrieve a random verse from dataset
 def get_random_sentence():
-  random_book = random.choice(books)
-  random_verse = random.choice(random_book)
+  random_verse = random.choice(verses)
   return random_verse["text"]
