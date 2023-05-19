a08ba251acf120bf86dfaf81158d9acd6c6eea72
Elkin Garcia <elkingarcia.11@gmail.com>
2023-05-19 12:13:39 -0400

    added validation to get document, delete from db

--- cloudfs.py
+++ cloudfs.py
@@ -3,23 +3,32 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from google.cloud.firestore_v1.base_query import FieldFilter
 
-from biblejson import verses
-
 # Data is array [string verse]
 def add_multiple(data):
     for d in data:
-        db.collection('verses').add({"text":d})
+        insert_into_db(d)
 
 # Document is string verse
 def insert_into_db(document):
-    db.collection('verses').add({"text":document})
+    if type(document) == str:
+        db.collection('verses').add({"text":document})
 
 # Delete document by string verse
 def delete_from_db(text):
-    # Note: Use of CollectionRef stream() is prefered to get()
-    docs = db.collection('verses').where(filter=FieldFilter('text', '==', text)).stream()
+    i = getDocument(text)
+    if i != None:
+        db.collection('verses').document(i).delete()
+
+def getCollectionLength() -> int:
+    c = db.collection('verses').size()
+    print(c)
+
+def getDocument(verse) -> str:
+    docs = db.collection('verses').where(filter=FieldFilter('text', '==', verse)).stream()
     for doc in docs:
-        db.collection('verses').document(doc.id).delete()
+        print(type(doc.id))
+        return doc.id
+    return None
 
 # Use a service account.
 cred = credentials.Certificate('keys/bible-twitter-bot-firebase-adminsdk-1mnqr-3ea928f05d.json')
@@ -28,11 +37,16 @@ app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-delete_from_db('testing')
 '''
+
+insert_into_db("TESTING")
+add_multiple(["2","2",1])
+from biblejson import verses
+
 for i in verses:
     insert_into_db(i["text"])
+db.collection('verses').document("").delete()
+delete_from_db('python delete example')
 
-delete_from_db('python insert example')
-delete_from_db('verse text')
+delete_from_db('TESTING')
 '''
